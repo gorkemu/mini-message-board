963c0b1c64e0ffeda93b1e49b3c8454178fa7f21
gorkemu <gorkemunal88@gmail.com>
2023-03-25 20:05:54 +0300

    use moment

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,17 @@
 var express = require("express");
 var router = express.Router();
+const moment = require("moment");
 
 const messages = [
   {
     text: "I am the manager",
     user: "Dwight",
-    added: new Date(),
+    added: moment().format("MMMM Do YYYY, h:mm:ss a"),
   },
   {
     text: "That's what she said",
     user: "Michael",
-    added: new Date(),
+    added: moment().format("MMMM Do YYYY, h:mm:ss a"),
   },
 ];
 /* GET home page. */
@@ -22,7 +23,7 @@ router.post("/new", (req, res) => {
   messages.push({
     text: req.body.text,
     user: req.body.user,
-    added: new Date(),
+    added: moment().format("MMMM Do YYYY, h:mm:ss a"),
   });
   res.redirect("/");
 });
